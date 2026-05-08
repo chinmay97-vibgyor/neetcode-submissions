1fd239bc7a5a1437cc082f0cb6b36d052cbad9f5
chinmay97-vibgyor <chinmay.sarkar2023@gmail.com>
2026-05-08 11:46:36 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+
+        for s in strs:
+            res += (str(len(s))+"~"+s)
+        
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res_str = []
+        i=0
+        while i<len(s):
+            j=i
+            while i<=j and s[j]!="~":
+                j=j+1
+            length=int(s[i:j])
+            wrd= s[j+1: j+1+length]
+            res_str.append(wrd)
+            i = j+1+length
+        return res_str
+
+                
+
